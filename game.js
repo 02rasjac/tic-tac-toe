1b36b1934956e3c400401f475788ca933a533c32
Rasmus Jacklin <rasmusjacklin2002@gmail.com>
2023-03-24 13:27:52 +0100

    Change how to select square

--- game.js
+++ game.js
@@ -1,11 +1,21 @@
-const gameboard = (() => {
-  const squares = document.querySelectorAll('.square');
+const game = (() => {
+  const gameboard = (() => {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach((s, i) => {
+      s.dataset.index = i;
+    });
+
+    const select = (square, player) => {
+      console.log({ square, player });
+    };
+
+    return { select };
+  })();
 
   const select = (e) => {
+    if (e.target.dataset.index == null) return;
     console.log(e.target);
   };
 
-  squares.forEach((s) => {
-    s.addEventListener('click', select);
-  });
+  document.addEventListener('click', select);
 })();
